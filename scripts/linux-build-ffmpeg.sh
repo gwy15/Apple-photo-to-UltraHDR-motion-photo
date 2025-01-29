ee84bd798fe23e309011f1d8979883e6dbb11a22
gwy15 <gwy15thu@gmail.com>
2025-01-29 19:05:55 +0800

    introduce ffmpeg

---
+++ scripts/linux-build-ffmpeg.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ex
+root=$(realpath "${1:-/}")
+path=$root/libffmpeg
+install=$(realpath "${2:-/opt/pkg-config}")
+echo "Building libffmpeg in $path"
+if [ ! -d "$path" ]; then
+    mkdir $path
+    git clone --depth 1 --branch release/7.1 https://github.com/ffmpeg/ffmpeg $path
+fi
+build=$path/build
+if [ ! -d "$build" ]; then
+    mkdir $build
+fi
+cd $path
+# rsmpeg only supports full build as of now. https://github.com/CCExtractor/rusty_ffmpeg/issues/128
+./configure --prefix=$install \
+    --enable-gpl --disable-programs \
+    --disable-doc --disable-network \
+    --disable-metal
+    # --disable-avdevice --disable-swscale --disable-postproc \
+    # --disable-avfilter
+make -j
