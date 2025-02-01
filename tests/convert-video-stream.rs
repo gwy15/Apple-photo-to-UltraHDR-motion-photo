7131cc4d6f157f680ca403b1bbaf2ef9a55e1be3
gwy15 <gwy15thu@gmail.com>
2025-02-01 22:35:47 +0800

    update tests

--- tests/convert-video-stream.rs
+++ tests/convert-video-stream.rs
@@ -3,7 +3,10 @@ fn main() {
     tracing_subscriber::fmt::init();
     motion::i2a::video::VideoAudioEncodeRequest {
         input: "./tests/IMG_3853.MOV".as_ref(),
+        // input: "./tests/IMG_3281.MOV".as_ref(),
         output: "./tests/output.mp4".as_ref(),
+        encoder: "ac3",
+        bit_rate: 96_000,
     }
     .execute()
     .unwrap();
