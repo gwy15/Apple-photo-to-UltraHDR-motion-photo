1c79ff81804318ee3e609c020cda2241ad197913
gwy15 <gwy15thu@gmail.com>
2025-07-10 11:10:11 +0000

    update ghw-build.sh, fix ffmpeg compile

--- scripts/ghw-build.sh
+++ scripts/ghw-build.sh
@@ -16,8 +16,12 @@ sudo cmake --install $root/libheif/build
 $scripts/linux-build-libuhdr.sh $root $install
 sudo cmake --install $root/libuhdr/build
 
+$scripts/linux-build-libffmpeg.sh $root $install
+cd $root/libffmpeg && make install && cd $scripts/..
+
 env PKG_CONFIG_PATH=$install/lib/pkgconfig PKG_CONFIG_LIBDIR=$install/lib \
     PKG_CONFIG_ALL_STATIC=true \
     TURBOJPEG_STATIC=1 TURBOJPEG_LIB_DIR=$install/lib TURBOJPEG_INCLUDE_PATH=$install/include \
+    FFMPEG_PKG_CONFIG_PATH=$install/lib/pkgconfig \
     cargo build --example aa-photo-bridge --release
 ldd target/release/examples/aa-photo-bridge
